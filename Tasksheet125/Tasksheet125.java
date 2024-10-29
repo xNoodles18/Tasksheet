f0b097c3e7d673635466712398ee68acaad594ec
xNoodles18 <jlgardiola18@gmail.com>
2024-10-29 12:21:33 +0000

    task125 done

---
+++ Tasksheet125/Tasksheet125.java
@@ -0,0 +1,31 @@
+package Tasksheet125;
+
+class Point {
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void print() {
+        System.out.println("(" + x + "," + y + ")");
+    }
+
+    // Your code goes here
+    public void scale() {
+        x /= 2; 
+        y /= 2; 
+    }
+}
+
+public class Tasksheet125 {
+    public static void main(String[] args) {
+        Point p = new Point(32, 32);
+        for (int i = 0; i < 5; i++) {
+            p.scale();
+            p.print();
+        }
+    }
+}
