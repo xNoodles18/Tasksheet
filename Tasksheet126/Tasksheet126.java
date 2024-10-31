42053e46a90dd04f88c637a9f13512bc0e7d9e7a
xNoodles18 <jlgardiola18@gmail.com>
2024-10-31 09:16:38 +0000

    task126

---
+++ Tasksheet126/Tasksheet126.java
@@ -0,0 +1,55 @@
+
+class Vehicle {
+
+    String make;
+    String model;
+    int year;
+
+    public Vehicle(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+}
+
+class Car extends Vehicle {
+
+    int numberOfDoors;
+
+    public Car(String make, String model, int year, int numberOfDoors) {
+        super(make, model, year);
+        this.numberOfDoors = numberOfDoors;
+    }
+
+    public void displayDetails() {
+        System.out.println("Car Details:");
+        System.out.println("Make: " + getMake());
+        System.out.println("Model: " + getModel());
+        System.out.println("Year: " + getYear());
+        System.out.println("Number of Doors: " + numberOfDoors);
+    }
+    
+}
+
+public class Tasksheet126 {
+    public static void main(String[] args) {
+
+        Car car = new Car("Toyota", "Camry", 2022, 4);
+        car.displayDetails();
+        
+    }
+
+}
